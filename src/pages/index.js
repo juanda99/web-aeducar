a815b33eeac21f47964cb2fbaca032e4f7f61638
juanda <juandacorreo@gmail.com>
2020-06-16 00:20:50 +0200

    change text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,12 @@ export default function Index({ data }) {
   return (
     <div>
       <h1>Aeducar</h1>
+      <h2>Plataforma de docencia online del Departamento de Educación, Cultura y Deporte del Gobierno de Aragón.</h2>
       <img src={logo} style={{ width: 357, height: 40 }} />
       <Img
         fluid={data.file.childImageSharp.fluid}
         alt="Captura pantalla aeducar"
       />
-      <h2>¿Qué es aeducar?</h2>
-      <p>Es la plataforma de docencia online del Departamento de Educación, Cultura y Deporte del Gobierno de Aragón.
-      Ha sido creada por docentes para toda la comunidad educativa.
-        Pretende ser una herramienta segura que dote de continuidad a la docencia online en todas las etapas educativas.</p>
-
       <h2>Acceder a aeducar</h2>
       <SearchSchool />
     </div>
